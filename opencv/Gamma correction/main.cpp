93f5602eed3fe2ee25abe9ed08a15f4a6e2ec549
marble703 <703342335@qq.com>
2024-03-17 16:21:48 +0800

    optimized a task

--- opencv/Gamma correction/main.cpp
+++ opencv/Gamma correction/main.cpp
@@ -4,16 +4,12 @@
 using namespace cv;
 using namespace std;
 
-int main()
-{
-    Mat image1, output_image, image1_gray;
-    image1 = imread("src.jpg");
-
-    cvtColor(image1, image1_gray, COLOR_BGR2GRAY);
-    imshow("image1_gray", image1_gray);
+void gamma_correct(const cv::Mat& img, cv::Mat& output_image){
+    cv::Mat  image1_gray;
+    cvtColor(img, image1_gray, COLOR_BGR2GRAY);
 
     output_image = image1_gray.clone();
-    double gamma =2;
+    double gamma = 0.5;
     for (int i = 0; i < image1_gray.rows; i++)
     {
         for (int j = 0; j < image1_gray.cols; j++)
@@ -24,10 +20,22 @@ int main()
             output_image.at<uchar>(i, j) = static_cast<uchar>(correctedPixelValue);
         }
     }
- 
-    cv::imshow("gamma.jpg", output_image);
-    cv::imwrite("gamma.jpg", output_image);
+}
 
+int main()
+{
+    Mat image1, image2, output_image1, output_image2;
+    image1 = imread("1.png");
+    image2 = imread("2.jpg");
+
+    gamma_correct(image1, output_image1);
+    gamma_correct(image2, output_image2);
+ 
+    cv::imshow("gamma1.jpg", output_image1);
+    cv::imshow("gamma2.jpg", output_image2);
+
+    cv::imwrite("gamma1.jpg", output_image1);
+    cv::imwrite("gamma2.jpg", output_image2);
     waitKey(0);
     return 0;
 }
